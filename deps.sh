dbb797363ad18d7aa71df098ba840387bc2bc29d
gilgil <gilgil@gilgil.net>
2015-10-25 03:18:54 +0900

    Add deps.sh

---
+++ deps.sh
@@ -0,0 +1,12 @@
+# opengl
+sudo apt-get install libglu1-mesa-dev
+
+# glog
+# sudo apt-get install libgoogle-glog-dev # gilgil temp 2015.10.25
+
+# gflags
+# sudo apt-get install libgflags-dev # gilgil temp 2015.10.25
+
+# libevent-dev
+# sudo apt-get install libevent-dev # gilgil temp 2015.10.25
+
